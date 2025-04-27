39de43d4af6895d2694cc5afe9ae9bf8912ab849
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-04-27 16:42:47 +0700

    destroy done

--- DailyCalorie/app/Http/Livewire/Calorie/Index.php
+++ DailyCalorie/app/Http/Livewire/Calorie/Index.php
@@ -10,6 +10,25 @@ class Index extends Component
 {
     use WithPagination;
 
+    /**
+     * destroy function
+     */
+    public function destroy($calorieId)
+    {
+        $calorie = Calorie::find($calorieId);
+
+        if($calorie) {
+            $calorie->delete();
+        }
+
+        //flash message
+        session()->flash('message', 'Data Berhasil Dihapus.');
+
+        //redirect
+        return redirect()->route('calorie.index');
+
+    }
+
     public function render()
     {
         return view('livewire.calorie.index', [
