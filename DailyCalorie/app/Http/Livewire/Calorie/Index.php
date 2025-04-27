862b347bc1020fac1ec093d0a2363e1d56e2d623
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-04-27 16:22:57 +0700

    index done

--- DailyCalorie/app/Http/Livewire/Calorie/Index.php
+++ DailyCalorie/app/Http/Livewire/Calorie/Index.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Calorie;
 
+use App\Models\Calorie;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.calorie.index');
+        return view('livewire.calorie.index', [
+            'calories' => Calorie::latest()->paginate(5)
+        ]);
     }
 }
